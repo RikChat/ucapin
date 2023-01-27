cef533a1381838c1e37310919231c3b8e3af3a2c
RikChat <108530206+RikChat@users.noreply.github.com>
2023-01-27 22:22:55 +0700

    Update config.js

--- scripts/config.js
+++ scripts/config.js
@@ -1,12 +1,12 @@
 const firebaseConfig = {
-  apiKey: "GANTI DENGAN PROJECT KAMU",
-  authDomain: "GANTI DENGAN PROJECT KAMU",
-  databaseURL: "GANTI DENGAN PROJECT KAMU",
-  projectId: "GANTI DENGAN PROJECT KAMU",
-  storageBucket: "GANTI DENGAN PROJECT KAMU",
-  messagingSenderId: "GANTI DENGAN PROJECT KAMU",
-  appId: "GANTI DENGAN PROJECT KAMU"
-};
+  apiKey: "AIzaSyAm8T4Wal5Uc5_sAogtHQ58YJFGy9E8nDc",
+    authDomain: "rikcapin-719f0.firebaseapp.com",
+    projectId: "rikcapin-719f0",
+    storageBucket: "rikcapin-719f0.appspot.com",
+    messagingSenderId: "269005947789",
+    appId: "1:269005947789:web:a3039bf3274e9402bbb3c4",
+    measurementId: "G-9Q6BSKZMG2"
+  };
 const app = firebase.initializeApp(firebaseConfig);
 const rdb = firebase.database();
 const auth = firebase.auth();
